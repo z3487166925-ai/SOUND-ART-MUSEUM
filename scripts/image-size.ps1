2e752d74a0b50ce44b77d8129b345892ef1adb81
z3487166925-ai <z3487166925@gmail.com>
2026-06-06 15:25:35 +0800

    Delete scripts directory

--- scripts/image-size.ps1
+++
@@ -1,7 +0,0 @@
-Add-Type -AssemblyName System.Drawing
-$assets = "C:\Users\ZHANG\.cursor\projects\e\assets"
-Get-ChildItem $assets -Filter "ChatGPT_Image_2026_6_6*" | ForEach-Object {
-  $i = [System.Drawing.Image]::FromFile($_.FullName)
-  Write-Output "$($_.Name) $($i.Width)x$($i.Height)"
-  $i.Dispose()
-}
